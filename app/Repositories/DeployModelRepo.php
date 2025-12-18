ed9809cf414850092bc4ead8fc1ff1387535751a
Kevin Ponce <kevin.ponce@ct.uneatlantico.es>
2025-12-18 11:56:27 +0100

    Add DeployModelRepo class with CRUD methods for Deploy model

---
+++ app/Repositories/DeployModelRepo.php
@@ -0,0 +1,35 @@
+<?php
+namespace App\Repositories;
+use App\Models\Deploy;
+class DeployModelRepo{
+    public function create(array $data): Deploy
+    {
+        return Deploy::create($data);
+    }
+
+    public function findById(int $id): ?Deploy
+    {
+        return Deploy::findOrFail($id);
+    }
+
+    public function update(Deploy $deploy, array $data): Deploy
+    {
+        $deploy->update($data);
+        return $deploy;
+    }
+
+    public function delete(Deploy $deploy): void
+    {
+        $deploy->delete();
+    }
+
+    public function findByGitConfigId(int $gitConfigId)
+    {
+        return Deploy::where('git_config_id', $gitConfigId)->get();
+    }
+
+    public function findByProjectId(int $projectId)
+    {
+        return Deploy::where('project_id', $projectId)->get();
+    }
+}
