f9748d6091091e81d5ad629e792d1be90b5cdcc3
Kevin Ponce <kevin.ponce@ct.uneatlantico.es>
2025-12-18 11:53:20 +0100

    Add ProjectRepo class with CRUD methods for Project model

---
+++ app/Repositories/ProjectRepo.php
@@ -0,0 +1,31 @@
+<?php
+namespace App\Repositories;
+use App\Models\Project;
+class ProjectRepo
+{
+    public function allForUser(int $userId)
+    {
+        return Project::where('user_id', $userId)->get();
+    }
+
+    public function create(array $data): Project
+    {
+        return Project::create($data);
+    }
+
+    public function findById(int $id): ?Project
+    {
+        return Project::findOrFail($id);
+    }
+
+    public function update(Project $project, array $data): Project
+    {
+        $project->update($data);
+        return $project;
+    }
+
+    public function delete(Project $project): void
+    {
+        $project->delete();
+    }
+}
